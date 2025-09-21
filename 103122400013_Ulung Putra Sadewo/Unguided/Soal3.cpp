69e8ae74bfe8632d76bc4f2fa1d03e59a52bf6b6
Ulung Putra S. <akhmad.b1103@gmail.com>
2025-09-21 11:33:24 +0700

    Add files via upload

---
+++ 103122400013_Ulung Putra Sadewo/Unguided/Soal3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float clo1_val, clo2_val, clo3_val, clo4_val;
+
+    cout << "Masukkan nilai CLO-1 : ";
+    cin >> clo1_val;
+    cout << "Masukkan nilai CLO-2 : ";
+    cin >> clo2_val;
+    cout << "Masukkan nilai CLO-3 : ";
+    cin >> clo3_val;
+    cout << "Masukkan nilai CLO-4 : ";
+    cin >> clo4_val;
+
+    cout << "\nNilai yang Anda masukkan adalah:" << endl;
+    cout << "CLO 1: " << clo1_val << endl;
+    cout << "CLO 2: " << clo2_val << endl;
+    cout << "CLO 3: " << clo3_val << endl;
+    cout << "CLO 4: " << clo4_val << endl;
+
+    return 0;
+}
---
+++ 103122400013_Ulung Putra Sadewo/Unguided/Soal6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+struct NilaiSTD {
+    float clo1;
+    float clo2;
+    float clo3;
+    float clo4;
+    float nilaiAkhir;
+    string indeks;
+};
+
+float hitungNilaiAkhir(NilaiSTD dataMhs) {
+    return (0.30 * dataMhs.clo1) + (0.30 * dataMhs.clo2) + (0.20 * dataMhs.clo3) + (0.20 * dataMhs.clo4);
+}
+
+int main() {
+    NilaiSTD mahasiswa;
+    
+    cout << "--- Input 4 Nilai CLO untuk Dihitung Nilai Akhirnya ---" << endl;
+    cout << "CLO 1: "; cin >> mahasiswa.clo1;
+    cout << "CLO 2: "; cin >> mahasiswa.clo2;
+    cout << "CLO 3: "; cin >> mahasiswa.clo3;
+    cout << "CLO 4: "; cin >> mahasiswa.clo4;
+
+    // Panggil fungsi untuk menghitung nilai akhirnya
+    float nilai_akhir_hasil = hitungNilaiAkhir(mahasiswa);
+    
+    cout << fixed << setprecision(2);
+    cout << "\nHasil perhitungan nilai akhir: " << nilai_akhir_hasil << endl;
+
+    return 0;
+}
